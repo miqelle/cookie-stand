2a103f181a1d8f71a240fa69daaa9f6110e33e51
miqelle <miqellec3@gmail.com>
2021-07-26 15:48:27 -0500

    random # generator notes added

--- app.js
+++ app.js
@@ -5,35 +5,35 @@ let Seattle =
     minCust: 23,
     maxCust: 65,
     avgCookieSale: 6.3,
-    custPerHr: Math.floor(Math.random()* 65) + 1////method to generate a random # of customers per hour
+    custPerHr: Math.floor(Math.random()* 65) + 1////method to generate a random # between 1-65 customers per hour
 }
 let Tokyo = 
 {
     minCust: 3,
     maxCust: 24,
     avgCookieSale: 1.2,
-    custPerHr: Math.floor(Math.random()* 24) + 1////method to generate a random # of customers per hour
+    custPerHr: Math.floor(Math.random()* 24) + 1////method to generate a random # between 1-24 customers per hour
 }
 let Dubai = 
 {
     minCust: 11,
     maxCust: 38,
     avgCookieSale: 3.7,
-    custPerHr: Math.floor(Math.random()* 38) + 1////method to generate a random # of customers per hour
+    custPerHr: Math.floor(Math.random()* 38) + 1////method to generate a random # between 1-38customers per hour
 }
 let Paris = 
 {
     minCust: 20,
     maxCust: 38,
     avgCookieSale: 2.3,
-    custPerHr: Math.floor(Math.random()* 38) + 1////method to generate a random # of customers per hour
+    custPerHr: Math.floor(Math.random()* 38) + 1////method to generate a random # between 1-38 customers per hour
 }
 let Lima = 
 {
     minCust: 2,
     maxCust: 16,
     avgCookieSale: 4.6,
-    custPerHr: Math.floor(Math.random()* 16) + 1////method to generate a random # of customers per hour
+    custPerHr: Math.floor(Math.random()* 16) + 1////method to generate a random # between 1-16 customers per hour
 }
 
 
