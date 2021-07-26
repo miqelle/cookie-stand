4c1e4bbaf5c03ff23fae2dff3733147b64571d51
miqelle <miqellec3@gmail.com>
2021-07-26 15:31:52 -0500

    first commit w/object literals for each store

---
+++ app.js
@@ -0,0 +1,56 @@
+//create object literals for each shop location
+
+let Seattle = 
+{
+    minCust: 23,
+    maxCust: 65,
+    avgCookieSale: 6.3
+
+}
+let Tokyo = 
+{
+    minCust: 3,
+    maxCust: 24,
+    avgCookieSale: 1.2
+}
+let Dubai = 
+{
+    minCust: 11,
+    maxCust: 38,
+    avgCookieSale: 3.7
+}
+let Paris = 
+{
+    minCust: 20,
+    maxCust: 38,
+    avgCookieSale: 2.3
+}
+let Lima = 
+{
+    minCust: 2,
+    maxCust: 16,
+    avgCookieSale: 4.6
+}
+
+
+
+
+
+//Stores min/max hourly customers
+
+
+
+
+//method to generate a random # of customers per hour
+
+
+
+//Calculate/store simulated amounts of cookies purchased
+//for each hr at each location using avg cookies purchased and the random # 
+//of customers generated
+
+
+
+
+//Calculate the sum of the hourly totals....Display lists on sales.html
+
