2ad2fb32dce53fc737e4b72f0f130c83e4e8c4db
miqelle <miqellec3@gmail.com>
2021-08-02 08:51:49 -0500

    added 8pm to time array

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 //array for store hours 
-const storeHrs = ['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm'];
+const storeHrs = ['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm', '8pm'];
 
 // total cookies per hour for all stores
 let totalCookiesPerHour = [];
